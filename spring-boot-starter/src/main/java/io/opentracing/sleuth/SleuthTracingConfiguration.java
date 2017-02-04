e3b858daeead8b303d34a3922df3da85d886f708
Brian Devins <brian.devins@coxautoinc.com>
2017-02-04 11:31:52 -0500

    Don't trample on someone elses Tracer

--- spring-boot-starter/src/main/java/io/opentracing/sleuth/SleuthTracingConfiguration.java
+++ spring-boot-starter/src/main/java/io/opentracing/sleuth/SleuthTracingConfiguration.java
@@ -1,6 +1,7 @@
 package io.opentracing.sleuth;
 
 import io.opentracing.Tracer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class SleuthTracingConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean
     public Tracer tracer() {
         return new SleuthTracer();
     }
